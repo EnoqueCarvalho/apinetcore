c8d55f2d2c998c490742ff5e2a5788417ca43ba6
Enoque Carvalho <enoque.carvalho5@gmail.com>
2021-02-04 15:00:07 -0300

    estrutura ddd criada, entidade usuario criada, pacotes mysql e sqlserver adicionados

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        private DateTime? _createAt;
+
+        public DateTime? CreateAt
+        {
+            get { return _createAt; }
+            set { _createAt = _createAt == null ? DateTime.UtcNow : value; }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+    }
+}
